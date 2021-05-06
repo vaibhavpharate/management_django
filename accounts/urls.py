7e777a386a7197e844527cc70f9cda8290a37c16
vaibhavpharate <vaibhavpharate999@gmail.com>
2021-05-06 07:37:07 +0530

    new_update

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',views.index,name="home"),
     path('create_order',views.create_order,name="create_order"),
     path('customer/<int:pk>',views.customer,name="customer"),
+    path('update_order/<int:pk>',views.update_order,name="update_order"),
 ]
