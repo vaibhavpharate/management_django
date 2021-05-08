4231edd1624ceee653d881259f7c76007fc3979a
vaibhavpharate <vaibhavpharate999@gmail.com>
2021-05-08 10:49:19 +0530

    new_commit

--- accounts/forms_file.py
+++ accounts/forms_file.py
@@ -5,3 +5,23 @@ class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = "__all__"
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class TagForm(ModelForm):
+    class Meta:
+        model = Tag
+        fields = "__all__"
