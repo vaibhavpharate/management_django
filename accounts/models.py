c5df7baa59593ab1a3f43594bbc6e1060942a614
vaibhavpharate <vaibhavpharate999@gmail.com>
2021-05-15 18:15:25 +0530

    15_may_2021_2

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -25,6 +26,7 @@ class Product(models.Model):
         return self.name
 
 class Customer(models.Model):
+    user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     email = models.EmailField()
     phone = models.CharField(max_length=200)
